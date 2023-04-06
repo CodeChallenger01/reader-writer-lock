7c603cb4ca603c14e5b194fb9a9174d845cb0f25
Manish Mishra <manish.mishra@knoldus.com>
2023-04-06 17:00:50 +0530

    Added Driver class

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -4,8 +4,8 @@ object Driver extends App {
   private val multipleThreads = new MultipleThreads
   /* A thread pool that takes two thread at a time to perform the operation */
   private val threadPoolExecutor = Executors.newFixedThreadPool(2)
-  threadPoolExecutor.execute(multipleThreads.threadWriteTwo)
   threadPoolExecutor.execute(multipleThreads.threadReadOne)
-  threadPoolExecutor.execute(multipleThreads.threadReadTwo)
   threadPoolExecutor.execute(multipleThreads.threadWriteOne)
+  threadPoolExecutor.execute(multipleThreads.threadWriteTwo)
+  threadPoolExecutor.execute(multipleThreads.threadReadTwo)
 }
